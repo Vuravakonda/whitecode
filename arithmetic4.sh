e6e8c85a4f79d0245565be130bbd41f44179015f
root <root@prasanthv.alclabs.in>
2023-04-28 18:29:46 +0500

    modifi

--- arithmetic4.sh
+++ arithmetic4.sh
@@ -2,6 +2,9 @@
 read -p "Enter the first number:" num1
 read -p "Enter the second number:" num2
 #Test if the first number is big 
+oljhy'p;
+[]\p;'
+[\]
 if (($num1>$num2))
 then 
    echo "given number $num1 is bigger than $num2"
